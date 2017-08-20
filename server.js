f937eee248907bc8a0f2543697f0e9f7c3c664f3
NaranNaran <30766652+NaranNaran@users.noreply.github.com>
2017-08-20 23:08:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ content:`<p>
 }
 };
 function createTemplate (data){
-    var title=data.title;
+    //var title=data.title;
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
