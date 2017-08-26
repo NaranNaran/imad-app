250a841a3853ccc669aa5b18dc27d5b1ca40cbf1
NaranNaran <30766652+NaranNaran@users.noreply.github.com>
2017-08-26 16:49:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,8 +103,17 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    //Get the name from the request
+    var name=req.params.name;
+    names.push(name);
+    //JSON:javascript object Notation
+res.send(JSON.stringify(names));
 });
 
 
