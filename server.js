61fe57fc13d6a9eb68cf26798efa3bc44303af6f
NaranNaran <30766652+NaranNaran@users.noreply.github.com>
2017-08-27 14:33:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var pool=require('pg').Pool;
 var config={
     user:'Naran',
-    database:'tarajstar2013',
+    database:'tarajstar201',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
